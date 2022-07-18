b5fb28cf4f3e9ebcef3cf51ae25b8e3622a8783f
PROXT <skyrimxts@gmail.com>
2022-07-18 15:37:17 +0300

    Добил дизайн

--- js/main.js
+++ js/main.js
@@ -55,4 +55,23 @@ menu.addEventListener('click', (e) => {
 	}
 });
 
+function onToggle(event) {
+	if (event.target.open) {
+		document.querySelectorAll(".accordion > details[open]").forEach((el) => {
+			// Исключаем из перебора елемент который мы только что открыли
+			if (el === event.target) {
+				return;
+			}
+
+			// Закрываем все остальные елементы <details>
+			el.open = false;
+		});
+	}
+}
+
+// Вешаем наблюдатель на все елементы <details> внутри акордиона
+document
+	.querySelectorAll(".accordion > details")
+	.forEach((el) => el.addEventListener("toggle", onToggle));
+
  
